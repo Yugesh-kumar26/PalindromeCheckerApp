97235e2e81c37263a1f3453d9df8c4968b9231da
Yugesh-kumar26 <yugeshmathanr@gmail.com>
2026-02-27 10:04:59 +0530

    UC10:Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,27 +2,23 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindromeRecursive(String str, int start, int end) {
-
-        if (start >= end) {
-            return true;
-        }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        return isPalindromeRecursive(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
-        if (isPalindromeRecursive(input, 0, input.length() - 1)) {
-            System.out.println("Result: \"" + input + "\" is a Palindrome (Recursive check).");
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        String reversed = "";
+        for (int i = normalized.length() - 1; i >= 0; i--) {
+            reversed += normalized.charAt(i);
+        }
+        if (normalized.equals(reversed)) {
+            System.out.println("Result: \"" + input + "\" is a Palindrome (Case-insensitive, space-ignored).");
         } else {
-            System.out.println("Result: \"" + input + "\" is NOT a Palindrome (Recursive check).");
+            System.out.println("Result: \"" + input + "\" is NOT a Palindrome (Case-insensitive, space-ignored).");
         }
 
         scanner.close();
